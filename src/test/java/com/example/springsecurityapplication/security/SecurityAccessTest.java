9f5975b7422d00ff105a1621f415030ae1000842
Kozlova Olga <naina_kievna@mail.ru>
2026-03-11 18:47:39 +0700

    test: use testcontainers db profile for springboot tests

--- src/test/java/com/example/springsecurityapplication/security/SecurityAccessTest.java
+++ src/test/java/com/example/springsecurityapplication/security/SecurityAccessTest.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 class SecurityAccessTest {
